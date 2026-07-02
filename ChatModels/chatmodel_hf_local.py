359bca26cd8234f1b27b61e8c85d34fddbd66c11
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-07-02 18:07:39 +0530

    code to run hugging face models locally

---
+++ ChatModels/chatmodel_hf_local.py
@@ -0,0 +1,19 @@
+import os
+
+os.environ["HF_HOME"] = "D:/huggingface_cache"
+
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "temperature": 0.5,
+        "max_new_tokens": 100
+    }
+)
+
+chat = ChatHuggingFace(llm=llm)
+
+response = chat.invoke("What is the capital of India?")
+print(response.content)
