172cc689bb93403259e9c7483bf84ca765e2477a
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-07-09 23:07:26 +0530

    learning about groq api

--- LLMs/llm.py
+++ LLMs/llm.py
@@ -1,10 +1,25 @@
-from langchain_openai import OpenAI
+import os
+from pathlib import Path 
+from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct")
+if not api_key:
+    raise ValueError("API key not found.")
 
-result = llm.invoke("What is the capital of India?")
+client = Groq(api_key=api_key)
 
-print(result)
+model = 'llama-3.3-70b-versatile'
+role="user"
+prompt="Do you know MSD ?"
+message={
+    "role":role,
+    "content":prompt
+}
+
+messages=[message]
+
+response = client.chat.completions.create(model=model, messages=messages)
+print(response.choices[0].message.content)
