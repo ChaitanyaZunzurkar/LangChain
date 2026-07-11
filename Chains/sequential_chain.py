58d20575e1c547f9f14fcd850e4f7685231b6b23
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-07-11 16:13:19 +0530

    implement a sequential chain langchain

---
+++ Chains/sequential_chain.py
@@ -0,0 +1,30 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+template1 = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"]
+)
+
+template2 = PromptTemplate(
+    template="Write a 5 line summary on the follwing text:\n {text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({
+    'topic': 'black hole'
+})
+
+print(result)
