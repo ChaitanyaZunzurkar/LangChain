882e4a6415157fcd825391a42dfe34d529e16659
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-07-16 23:23:14 +0530

    Runnable sequence and runnable parallel

---
+++ Runnable/runnable_sequence.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+result = chain.invoke({
+    'topic': 'AI'
+})
+
+print(result)
